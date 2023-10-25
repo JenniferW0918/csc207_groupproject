68d5088335c6be330b98725a89bc23156118fc24
JenniferW0918 <nnjennifer.wang@mail.utoronto.ca>
2023-10-25 19:06:03 -0400

    User entity created

--- src/entity/User.java
+++ src/entity/User.java
@@ -1,12 +1,24 @@
 package entity;
 
-import java.time.LocalDateTime;
+public class User {
+    private final String name;
+    private final String username;
+    private final String email;
+    private final String password;
+    private final String location;
 
-public interface User {
+    public User(String name, String username, String email, String password,
+                String location) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.location = location;
+    }
 
-    String getName();
-
-    String getPassword();
-
-    LocalDateTime getCreationTime();
+    public String getName() {return name;}
+    public String getUsername() {return username;}
+    public String getEmail() {return email;}
+    public String getPassword() {return password;}
+    public String getLocation() {return location;}
 }
