47ad3806892caf7c605c21a70a71dda604535ab0
banke00 <baankeakande078@gmail.com>
2023-11-20 09:25:17 -0500

    updated files

--- src/interface_adapter/search_name/SearchNameViewModel.java
+++ src/interface_adapter/search_name/SearchNameViewModel.java
@@ -1,4 +1,47 @@
 package interface_adapter.search_name;
 
-public class SearchNameViewModel {
+import interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class SearchNameViewModel extends ViewModel {
+
+    public static final String TITLE_LABEL = "Search View";
+    public static final String TERM_LABEL = "Type Term";
+    public static final String LOCATION_LABEL = "Type Location";
+
+    public static final String SEARCH_NAME_BUTTON_LABEL = "Search Name";
+
+    public static final String CLEAR_BUTTON_LABEL = "Clear";
+
+    private SearchNameState state = new SearchNameState();
+
+    public SearchNameViewModel() {
+        super("search name");
+    }
+
+    public void setState(SearchNameState state) {
+        this.state = state;
+    }
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+
+
+    //Signup Presenter will call to let the ViewModel know to alert the View
+
+    @Override
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    public SearchNameState getState() {
+        return state;
+    }
+
 }
--- src/use_case/search_name/SearchNameOutputData.java
+++ src/use_case/search_name/SearchNameOutputData.java
@@ -16,3 +16,5 @@ public class SearchNameOutputData {
         return searchNameResult;
     }
 }
+
+
