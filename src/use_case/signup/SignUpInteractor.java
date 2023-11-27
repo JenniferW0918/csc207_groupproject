02982f3db748bfbeeebc4f1c958fcb62c8722c77
banke00 <baankeakande078@gmail.com>
2023-11-27 09:30:33 -0500

    updated files

--- src/use_case/signup/SignUpInteractor.java
+++ src/use_case/signup/SignUpInteractor.java
@@ -3,8 +3,6 @@ package use_case.signup;
 import entity.User;
 import entity.UserFactory;
 
-import java.time.LocalDateTime;
-
 public class SignUpInteractor implements SignUpInputBoundary {
     final SignUpDataAccessInterface userDataAccessObject;
     final SignUpOutputBoundary userPresenter;
@@ -20,11 +18,11 @@ public class SignUpInteractor implements SignUpInputBoundary {
 
     @Override
     public void execute(SignUpInputData signupInputData) {
-        if (userDataAccessObject.existsByName(signupInputData.getUsername())) {
+        if (userDataAccessObject.userExistsByName(signupInputData.getUsername())) {
             userPresenter.prepareFailView("User already exists.");
         } else {
             User user = userFactory.create("name", signupInputData.getUsername(), signupInputData.getPassword());
-            userDataAccessObject.save(user);
+            userDataAccessObject.saveUser(user);
 
             SignUpOutputData signupOutputData = new SignUpOutputData(user.getName(), false);
             userPresenter.prepareSuccessView(signupOutputData);
