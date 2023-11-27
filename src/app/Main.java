25f7f6deefd19187ceed4f8b74b0c0b3e8fceb61
audreyfoy <audrey.foy@mail.utoronto.ca>
2023-11-27 13:41:23 -0500

    created an AddBusiness use case factory for Main

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,6 +2,7 @@ package app;
 
 import data_access.Accounts;
 import data_access.SearchNameDataAccessObject;
+import entity.BusinessAccountFactoryInterface;
 import entity.SearchNameResult;
 import interface_adapter.add_business.AddBusinessAccountController;
 import interface_adapter.add_business.AddBusinessAccountViewModel;
@@ -14,7 +15,9 @@ import interface_adapter.search_name.SearchNameViewModel;
 import interface_adapter.login.LoginViewModel;
 import interface_adapter.ViewManagerModel;
 import interface_adapter.signup.AddBusinessAccountPresenter;
+import use_case.add_business.AddBusinessAccountDataAccessInterface;
 import use_case.add_business.AddBusinessAccountInteractor;
+import use_case.add_business.AddBusinessAccountOutputBoundary;
 import use_case.add_user.AddUserInteractor;
 import use_case.search_name.SearchNameInputData;
 import view.*;
@@ -48,33 +51,25 @@ public class Main {
         SearchedNameViewModel searchedNameViewModel = new SearchedNameViewModel();
         AddUserViewModel addUserViewModel = new AddUserViewModel();
         AddBusinessAccountViewModel addBusinessAccountViewModel = new AddBusinessAccountViewModel();
-        LoginViewModel loginViewModel = new LoginViewModel();
 
 
         //Making Data Access Objects
         SearchNameDataAccessObject searchNameDataAccessObject = new SearchNameDataAccessObject();
         Accounts userDataAccessObject = new Accounts();
 
+        // Making business data access object:
+        Accounts businessDataAccessObject = new Accounts(); // this isn't right, but not sure how to create the business
+        // data access object or what the business data access object is meant to be.
+
         // Making an AddUserView object
         AddUserPresenter addUserPresenter = new AddUserPresenter(viewManagerModel, addUserViewModel, loginViewModel);
         AddUserInteractor addUserInteractor = new AddUserInteractor(userAddUserUseCaseInteractor);
         AddUserController addUserController = new AddUserController(userAddUserUseCaseInteractor);
-        // testing commits/pulls
-        // ugh!
-        // why wont this commit
+
 
         AddUserView addUserView = new AddUserView(addUserController, addUserViewModel);
         views.add(addUserView, addUserView.viewName);
 
-        // Making an AddBusinessAccountView object
-        AddBusinessAccountPresenter addBusinessAccountPresenter = new AddBusinessAccountPresenter(viewManagerModel,
-                addBusinessAccountViewModel, loginViewModel);
-        AddBusinessAccountInteractor addBusinessAccountInteractor = new AddBusinessAccountInteractor(userAddBusinessUseCaseInteractor);
-        AddBusinessAccountController addBusinessAccountController = new AddBusinessAccountController(userAddBusinessUseCaseInteractor);
-
-        AddBusinessAccountView addBusinessAccountView = new AddBusinessAccountView(addBusinessAccountController,
-                addBusinessAccountViewModel);
-        views.add(addBusinessAccountView, addBusinessAccountView.viewName);
 
         // Making searchNameView
         SearchNameView searchNameView = SearchNameUseCaseFactory.createSearchNameView(viewManagerModel,
@@ -85,9 +80,9 @@ public class Main {
         SearchedNameView searchedNameView = new SearchedNameView(searchedNameViewModel, viewManagerModel);
         views.add(searchedNameView, searchedNameView.viewName);
 
-        // Making SignUpView
-        SignUpView signUpView = SignUpUseCaseFactory.create(viewManagerModel, loginViewModel, signUpViewModel, userDataAccessObject);
-        views.add(signUpView, signUpView.viewName);
+        // Making an AddBusinessAccount View
+        AddBusinessAccountView addBusinessAccountView = AddBusinessUseCaseFactory.create(viewManagerModel,
+                searchNameViewModel, addBusinessAccountViewModel, businessDataAccessObject);
 
 
 //    THE DEFAULT VIEW
