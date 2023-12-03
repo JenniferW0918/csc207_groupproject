d282cbd633bcffb43cb8cecae91e6abaee03fedd
banke00 <baankeakande078@gmail.com>
2023-12-03 15:36:18 -0500

    updated files

--- src/use_case/add_user/AddUserDataAccessInterface.java
+++ src/use_case/add_user/AddUserDataAccessInterface.java
@@ -3,7 +3,7 @@ package use_case.add_user;
 import entity.User;
 
 public interface AddUserDataAccessInterface {
-    boolean userExistsByName(String identifier);
+    boolean userExistsByUsername(String identifier);
 
     void saveUser(User user);
 }
