756e607635985f3f471470916546c333f7395fe9
audreyfoy <audrey.foy@mail.utoronto.ca>
2023-12-07 20:16:57 -0500

    javadoc for my entities

--- src/entity/BusinessAccount.java
+++ src/entity/BusinessAccount.java
@@ -1,13 +1,48 @@
 package entity;
 
-
+/**
+ * The BusinessAccount class represents a business user account with attributes such as username, name, password,
+ * address, and category. Instances of this class are immutable once initialized. It provides methods to access these
+ * attributes.
+ *
+ * @author audrey
+ * @version 1.0
+ */
 public class BusinessAccount {
+    /**
+     * The unique username associated with the business account.
+     */
     private final String username;
+
+    /**
+     * The name of the business associated with the account.
+     */
     private final String name;
+
+    /**
+     * The password associated with the business account.
+     */
     private final String password;
+
+    /**
+     * The address of the business associated with the account.
+     */
     private final String address;
+
+    /**
+     * The category of the business associated with the account.
+     */
     private final String category;
 
+    /**
+     * Constructs a BusinessAccount object with the specified attributes.
+     *
+     * @param username The unique username associated with the business account.
+     * @param name     The name of the business associated with the account.
+     * @param password The password associated with the business account.
+     * @param address  The address of the business associated with the account.
+     * @param category The category of the business associated with the account.
+     */
     public BusinessAccount(String username, String name, String password, String address,
                            String category) {
         this.username = username;
--- src/entity/BusinessAccountFactoryInterface.java
+++ src/entity/BusinessAccountFactoryInterface.java
@@ -1,5 +1,23 @@
 package entity;
 
+/**
+ * The BusinessAccountFactoryInterface provides an interface for creating instances of the BusinessAccount class.
+ * Implementing classes are responsible for defining the method to create a new business account with specified attributes.
+ *
+ * @author audrey
+ * @version 1.0
+ */
 public interface BusinessAccountFactoryInterface {
-    BusinessAccount create(String username, String name, String password, String address, String categories);
+
+    /**
+     * Creates and returns a new instance of the BusinessAccount class with the specified attributes.
+     *
+     * @param username The unique username associated with the business account.
+     * @param name     The name of the business associated with the account.
+     * @param password The password associated with the business account.
+     * @param address  The address of the business associated with the account.
+     * @param category The category of the business associated with the account.
+     * @return A new BusinessAccount instance with the specified attributes.
+     */
+    BusinessAccount create(String username, String name, String password, String address, String category);
 }
